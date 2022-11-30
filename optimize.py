328b9eade43190191ec1bb2e562f7af771da2f97
Lisa Zacarias <zacarias@slac.stanford.edu>
2022-11-29 18:05:54 -0800

    some file cleanup

--- optimize.py
+++
@@ -1,8 +0,0 @@
-from lcls_tools.superconducting.scLinac import ALL_CRYOMODULES
-
-from gain_linac import GAIN_CRYOMODULES
-
-for cm_name in ALL_CRYOMODULES:
-    cm = GAIN_CRYOMODULES[cm_name]
-    for cav in cm.cavities.values():
-        cav.search()
